354ea0bc25d0ab734b56c5c6b3a501ff9e287363
Ahmad Fikri <43110908+AhmadFikriAS@users.noreply.github.com>
2023-03-23 17:22:14 +0800

    Update secret_file.dart

--- lib/secret_file.dart
+++ lib/secret_file.dart
@@ -1,2 +1,2 @@
-const openAiApiKey = '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';
-const openAiOrg = 'org-G9bwObUGo2fRjPS0VuAzHfLA';
+const openAiApiKey = 'your_apikey';
+const openAiOrg = 'your_apiOrg';
